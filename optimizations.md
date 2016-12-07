cbbe4e1508155abb238c01854952be39a2f84b28
Faraz Syed <hello@farazsyed.com>
2016-12-07 16:53:53 -0700

    Add functions as model variables, cache the functions when updating the view, rationalize the deep proxy model, add documentation as comments

--- optimizations.md
+++ optimizations.md
@@ -7,3 +7,5 @@ don't pass around the stack in createDeepProxy
 DONE recursively define proxies - this can catch new properties that were not get/set before
 
 debounce/throttle changes
+
+if I don't have a match on a select, I should select a blank
--- src/rudy.js
+++ src/rudy.js
@@ -1,25 +1,63 @@
-rudy = function (id, component) {
+'use strict';
+
+var moduleTarget = (typeof global === 'undefined') ? window : global;
+
+moduleTarget.rudy = function (id, component) {
     component.template = component.template || document.getElementById(id).innerHTML;
-    component.data = createDeepProxy(component.data, viewRefresher(id, component));
-    if (typeof component.create === 'function') component.create.call(component.data);
-    viewRefresher(id, component)();
+
+    var __original_data__ = Object.assign({}, component.data);
+    var __proxy__ = null;
+
+    Object.defineProperty(component, 'data', {
+        get: function () {
+            return __proxy__;
+        },
+        set: function (value) {
+            console.log('SET DATA', value);
+            var __new_data__ = Object.assign({}, value);
+            __proxy__ = proxyfull(__new_data__, viewRefresher(id, this));
+            if (typeof this.create === 'function') {
+                this.__create__ = this.create;
+                this.create.call(this.data);
+                delete this.create;
+            }
+            viewRefresher(id, this)();
+        },
+    });
+
+    component.data = __original_data__;
+
     return component;
 }
 
 function viewRefresher(id, component) {
     return function refreshView() {
         if (Object.getOwnPropertyNames(component.data).length > 0 && component.template !== '') {
-            substituteValues(component.template, component.data, function (error, result) {
-                if (result) document.getElementById(id).innerHTML = result;
-            });
-            findAllInputElements(document.getElementById(id)).forEach(function (inputElement) {
-                var value = getProperty(component.data, inputElement.getAttribute('name'));
-                if (typeof value !== 'undefined') {
-                    if (inputElement.getAttribute('type') == 'checkbox') inputElement.checked = value;
-                    else inputElement.value = value;
+
+            var cache = {};
+
+            substituteValues(
+                component.template,
+                component.data,
+                function (error, result, cachedValues) {
+                    if (result) {
+                        document.getElementById(id).innerHTML = result;
+                        cache = cachedValues;
+                    }
                 }
-            });
+            );
+
+            findAllInputElements(document.getElementById(id))
+                .forEach(function (inputElement) {
+                    var value = cache[inputElement.getAttribute('name')] || getProperty(component.data, inputElement.getAttribute('name'));
+                    if (typeof value !== 'undefined') {
+                        if (typeof value === 'function') value = value.call(Object.assign({}, component.data));
+                        if (inputElement.getAttribute('type') === 'checkbox') inputElement.checked = value;
+                        else inputElement.value = value;
+                    }
+                });
         }
+        return true;
     };
 }
 
@@ -27,27 +65,30 @@ function substituteValues(initialHTML, data, callback) {
     var atleastOneValidMatchFound = false;
     var html = initialHTML;
     var matches = html.match(/{{(.+?)}}/g);
+    var cache = {};
     if (matches) {
         matches.forEach(function (match) {
             var prop = match.replace(/[{}]/g, '').trim();
             var value = getProperty(data, prop);
             if (typeof value !== 'undefined') {
+                if (typeof value === 'function') cache[prop] = value = value.call(Object.assign({}, data));
                 atleastOneValidMatchFound = true;
                 html = html.replace(match, value);
             }
         });
     }
-    if (atleastOneValidMatchFound && callback) callback(null, html);
+    if (atleastOneValidMatchFound && callback) callback(null, html, cache);
     else if (callback) callback(true);
 }
 
 function getProperty(source, property) {
-    return property
+    if (source && property) return property
         .split('.')
         .reduce(function (initial, key) {
             if (typeof initial === 'undefined') return undefined;
             else return initial[key];
         }, source);
+    else return undefined;
 }
 
 function findAllInputElements(rootElement) {
@@ -57,7 +98,7 @@ function findAllInputElements(rootElement) {
     do {
         elementsRemaining = [];
         [].forEach.call(elementsToCheck, function (node) {
-            if (node.tagName === 'INPUT') inputs.push(node);
+            if (['INPUT', 'RANGE', 'SELECT', 'TEXTAREA'].indexOf(node.tagName) > -1) inputs.push(node);
             else if (node.childNodes.length > 0) elementsRemaining.push.apply(elementsRemaining, node.childNodes);
         })
         elementsToCheck = elementsRemaining;
@@ -66,13 +107,13 @@ function findAllInputElements(rootElement) {
     return inputs;
 }
 
-function createDeepProxy(original, setter, stack) {
-    if (typeof stack === 'undefined') stack = 'root';
+function proxyfull(original, setter, stack) {
+    if (typeof stack === 'undefined') stack = '';
     var newOriginal = Object.assign({}, original);
 
     Object.keys(newOriginal).forEach(function (key) {
         if (typeof newOriginal[key] === "object") {
-            newOriginal[key] = createDeepProxy(newOriginal[key], setter, stack + '.' + key);
+            newOriginal[key] = proxyfull(newOriginal[key], setter, stack + '/' + key);
         }
     });
 
@@ -82,10 +123,15 @@ function createDeepProxy(original, setter, stack) {
                 return target[property];
             },
             set: function (target, property, value, receiver) {
+                console.log('SET', stack + '/' + property, value)
 
-                if (typeof value === 'object') target[property] = createDeepProxy(value, setter, stack + '.' + property);
-                else target[property] = value;
-                setter(value, stack + '.' + property);
+                if (typeof value === 'object') target[property] = proxyfull(value, setter, stack + '/' + property);
+                else {
+                    target[property] = value;
+                    original[property] = value;
+                }
+                return setter(value, stack + '/' + property);
             }
-        });
+        }
+    );
 }
